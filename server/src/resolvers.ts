bd0d6594c58211b1498ef39d602927ea5a03d8b4
wwJavid <javababayev@gmail.com>
2023-03-05 16:52:52 +0200

    resolver seperating

---
+++ server/src/resolvers.ts
@@ -0,0 +1,6 @@
+import queryTwitterResolvers from "./resolvers/Query"
+
+const resolvers = {
+    Query: queryTwitterResolvers,
+}
+export default resolvers
---
+++ server/src/resolvers/Query.ts
@@ -0,0 +1,17 @@
+const queryTwitterResolvers = {
+    currentUser: () => {
+        return {
+            id: "123",
+            name: "John Doe",
+            handle: "johndoe",
+            coverUrl: "",
+            avatarUrl: "",
+            createdAt: "",
+            updatedAt: "",
+        }
+    },
+    suggestions: () => {
+        return []
+    },
+}
+export default queryTwitterResolvers
