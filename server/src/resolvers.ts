4dd71272896810d419852de41eba61f8e8fb4d01
wwJavid <javababayev@gmail.com>
2023-03-05 19:39:07 +0200

    resolver type generating

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,6 +1,7 @@
+import { Resolvers } from "./resolvers-types.generated"
 import queryTwitterResolvers from "./resolvers/Query"
 
-const resolvers = {
+const resolvers: Resolvers = {
     Query: queryTwitterResolvers,
 }
 export default resolvers
--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,4 +1,6 @@
-const queryTwitterResolvers = {
+import { QueryResolvers } from "../resolvers-types.generated"
+
+const queryTwitterResolvers: QueryResolvers = {
     currentUser: () => {
         return {
             id: "123",
@@ -11,7 +13,23 @@ const queryTwitterResolvers = {
         }
     },
     suggestions: () => {
-        return []
+        return [
+            {
+                name: "TypeScript Project",
+                handle: "TypeScript",
+                avatarUrl: "http://localhost:3000/static/ts-logo.png",
+                reason: "Because you follow @MichaelLNorth",
+                id: "1",
+            },
+            {
+                name: "jQuery",
+                handle: "jquery",
+                avatarUrl:
+                    "http://localhost:3000/static/jquery-logo.jpeg",
+                reason: "Because you follow @FrontendMasters",
+                id: "2",
+            },
+        ]
     },
 }
 export default queryTwitterResolvers
